6aa3907113d927d55ce22971d9e0e8e8aa883436
LuckyMan012359 <rorifloris@gmail.com>
2025-03-12 15:20:19 +0000

    feat: complete HVAC page

---
+++ src/pages/HVAC/components/TempListCard.tsx
@@ -0,0 +1,69 @@
+import TempListItemCard from "./TempListItemCard";
+
+type TagLabel = "auto" | "off" | "cool" | "heat";
+
+type TempItem = {
+  tempStatus: TagLabel;
+  itemName: string;
+  currentTemp: number;
+};
+
+const TempItemData: TempItem[] = [
+  {
+    tempStatus: "auto",
+    itemName: "Golf simulator",
+    currentTemp: 76,
+  },
+  {
+    tempStatus: "off",
+    itemName: "Media room",
+    currentTemp: 60,
+  },
+  {
+    tempStatus: "cool",
+    itemName: "Home theater",
+    currentTemp: 80,
+  },
+  {
+    tempStatus: "auto",
+    itemName: "Main bedroom",
+    currentTemp: 76,
+  },
+  {
+    tempStatus: "cool",
+    itemName: "Main ensuite",
+    currentTemp: 84,
+  },
+  {
+    tempStatus: "heat",
+    itemName: "study",
+    currentTemp: 75,
+  },
+  {
+    tempStatus: "auto",
+    itemName: "Library",
+    currentTemp: 69,
+  },
+];
+
+const Card = () => {
+  return (
+    <div className="card w-full h-auto bg-[#07182E] rounded-2xl overflow-hidden relative transition-all duration-300 hover:shadow-[0_0_30px_rgba(0,183,255,0.5)]">
+      <div className="card-content h-[calc(100vh-250px)] p-4 relative z-10 flex flex-col gap-5">
+        <h6>GLOBAL CLIMATES</h6>
+        <div className="flex flex-col gap-3 py-5 px-2 justify-between overflow-y-scroll">
+          {TempItemData.map((item, index) => (
+            <TempListItemCard
+              key={index}
+              tempStatus={item.tempStatus}
+              itemName={item.itemName}
+              currentTemp={item.currentTemp}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/pages/HVAC/index.tsx
+++ src/pages/HVAC/index.tsx
@@ -1,10 +1,17 @@
+import ClimateControllCard from "./components/AllTempControlCard";
+import Card from "./components/TempListCard";
+
 const HVAC = () => {
-    return (
-        <div>
-            <h1>HVAC</h1>
-            <p>Heating, Ventilation, and Air Conditioning</p>
-        </div>
-    );
+  return (
+    <div className="flex justify-between gap-10">
+      <div className="w-1/3">
+        <ClimateControllCard currentTemp={72} targetTemp={69} />
+      </div>
+      <div className="w-2/3">
+        <Card />
+      </div>
+    </div>
+  );
 };
 
 export default HVAC;
