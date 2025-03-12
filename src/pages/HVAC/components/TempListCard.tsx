55f615adfbfa9ac1460560e74cf2bbd329b3c277
LuckyMan012359 <rorifloris@gmail.com>
2025-03-12 16:08:13 +0000

    fix: update temp list card's height

--- src/pages/HVAC/components/TempListCard.tsx
+++ src/pages/HVAC/components/TempListCard.tsx
@@ -51,7 +51,7 @@ const Card = () => {
     <div className="card w-full bg-[#07182E] rounded-2xl relative transition-all duration-300 hover:shadow-[0_0_30px_rgba(0,183,255,0.5)]">
       <div className="card-content h-[calc(100vh-250)] p-4 relative z-10 flex flex-col gap-5">
         <h6>GLOBAL CLIMATES</h6>
-        <div className="flex flex-col gap-3 h-[calc(100vh-300px)] py-5 px-2 justify-between overflow-auto">
+        <div className="flex flex-col gap-3 h-[calc(100vh-330px)] py-5 px-2 justify-between overflow-auto">
           {TempItemData.map((item, index) => (
             <TempListItemCard
               key={index}
