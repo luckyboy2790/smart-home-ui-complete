dc011eab0e8f7743147ef2dad6f05b1c349d0190
LuckyMan012359 <rorifloris@gmail.com>
2025-03-13 19:55:43 +0000

    feat: complete dashboard page

---
+++ src/pages/Home/components/Lights/LightSelect.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import {
+  Listbox,
+  ListboxButton,
+  ListboxOption,
+  ListboxOptions,
+} from "@headlessui/react";
+import { IoIosArrowDown } from "react-icons/io";
+
+const rooms = ["Master Bedroom", "Living Room", "Guest Room", "Kitchen"];
+
+const CustomSelect = ({
+  setRoomName,
+}: {
+  setRoomName: React.Dispatch<React.SetStateAction<string>>;
+}) => {
+  const [selectedRoom, setSelectedRoom] = useState(rooms[0]);
+
+  return (
+    <div className="relative text-white z-10 w-50">
+      <Listbox
+        value={selectedRoom}
+        onChange={(value) => {
+          setSelectedRoom(value);
+          setRoomName(value);
+        }}
+      >
+        <ListboxButton className="flex w-full items-center justify-between gap-2 px-4 py-2 text-sm bg-gray-900 rounded-md">
+          <span>{selectedRoom}</span>
+          <IoIosArrowDown className="text-blue-500" />
+        </ListboxButton>
+        <ListboxOptions className="absolute mt-2 w-full bg-gray-800 text-white rounded-md shadow-lg">
+          {rooms.map((room, index) => (
+            <ListboxOption
+              key={index}
+              value={room}
+              className="px-4 py-2 cursor-pointer text-sm hover:bg-gray-700"
+            >
+              {room}
+            </ListboxOption>
+          ))}
+        </ListboxOptions>
+      </Listbox>
+    </div>
+  );
+};
+
+export default CustomSelect;
---
+++ src/pages/Home/components/Media/MediaTVSelect.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import {
+  Listbox,
+  ListboxButton,
+  ListboxOption,
+  ListboxOptions,
+} from "@headlessui/react";
+import { IoIosArrowDown } from "react-icons/io";
+
+const rooms = ["Upstairs TV Room"];
+
+const MediaTVSelect = () => {
+  const [selectedRoom, setSelectedRoom] = useState(rooms[0]);
+
+  return (
+    <div className="relative text-white z-10 w-50">
+      <Listbox
+        value={selectedRoom}
+        onChange={(value) => {
+          setSelectedRoom(value);
+        }}
+      >
+        <ListboxButton className="flex w-full items-center justify-between gap-2 px-4 py-2 text-sm bg-gray-900 rounded-md">
+          <span>{selectedRoom}</span>
+          <IoIosArrowDown className="text-blue-500" />
+        </ListboxButton>
+        <ListboxOptions className="absolute mt-2 w-full bg-gray-800 text-white rounded-md shadow-lg">
+          {rooms.map((room, index) => (
+            <ListboxOption
+              key={index}
+              value={room}
+              className="px-4 py-2 cursor-pointer text-sm hover:bg-gray-700"
+            >
+              {room}
+            </ListboxOption>
+          ))}
+        </ListboxOptions>
+      </Listbox>
+    </div>
+  );
+};
+
+export default MediaTVSelect;
