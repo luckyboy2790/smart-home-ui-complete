703ee6eb18aaa5371e67b359aa7775d586f52514
devstar222696 <david.jamar.richardson77@gmail.com>
2025-03-16 17:11:19 -0500

    removed unused useEffect on AppContext

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect } from "react";
+import { createContext, useContext } from "react";
 import { AppContextType } from "../types";
 import { AppProviderProps } from "../types";
 import { useState } from "react";
@@ -6,22 +6,22 @@ import { useState } from "react";
 const AppContext = createContext<AppContextType | undefined>(undefined);
 
 export const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
-  const [title, setTitle] = useState<string | null>(null);
-  const [alarmStatus, setAlarmStatus] = useState<boolean>(false);
+    const [title, setTitle] = useState<string | null>(null);
+    const [alarmStatus, setAlarmStatus] = useState<boolean>(false);
 
-  return (
-    <AppContext.Provider
-      value={{ title, setTitle, alarmStatus, setAlarmStatus }}
-    >
-      {children}
-    </AppContext.Provider>
-  );
+    return (
+        <AppContext.Provider
+            value={{ title, setTitle, alarmStatus, setAlarmStatus }}
+        >
+            {children}
+        </AppContext.Provider>
+    );
 };
 
 export const useAppContext = (): AppContextType => {
-  const context = useContext(AppContext);
-  if (!context) {
-    throw new Error("useAppContext must be used within an AppProvider");
-  }
-  return context;
+    const context = useContext(AppContext);
+    if (!context) {
+        throw new Error("useAppContext must be used within an AppProvider");
+    }
+    return context;
 };
