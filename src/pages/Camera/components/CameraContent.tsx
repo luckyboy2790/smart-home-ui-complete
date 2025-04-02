984c46a365fc21de335edd70a760b97cf1b66c17
devstar222696 <david.jamar.richardson77@gmail.com>
2025-04-02 03:55:24 -0500

    hided the PZT control on camera

--- src/pages/Camera/components/CameraContent.tsx
+++ src/pages/Camera/components/CameraContent.tsx
@@ -4,12 +4,18 @@ import CameraZoomSection from "./CameraZoomSection";
 
 const CameraContent = () => {
   return (
-    <div className="w-full h-full overflow-auto bg-[#1b1b1bb0] flex lg:flex-row flex-col justify-between items-center relative">
+    <div className="w-full h-full overflow-auto bg-[#1b1b1bb0] flex lg:flex-row flex-col justify-between items-center relative group">
       <CameraSelect />
-      <div className="grow h-full absolute lg:relative lg:top-0 lg:left-0">
-        <CameraControllerSection />
+
+      <div className="grow h-full relative">
+        <div className="absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-500 delay-100 pointer-events-none group-hover:pointer-events-auto">
+          <CameraControllerSection />
+        </div>
       </div>
-      <CameraZoomSection />
+
+      <div className="opacity-0 group-hover:opacity-100 transition-opacity duration-500 delay-100 pointer-events-none group-hover:pointer-events-auto">
+        <CameraZoomSection />
+      </div>
     </div>
   );
 };
