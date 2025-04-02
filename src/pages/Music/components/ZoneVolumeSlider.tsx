2edabde919f949e95c2969fd72b7c38691e73095
LuckyBoy012359 <rorifloris@gmail.com>
2025-04-03 00:51:30 +0100

    fix: update music share card

--- src/pages/Music/components/ZoneVolumeSlider.tsx
+++ src/pages/Music/components/ZoneVolumeSlider.tsx
@@ -1,26 +1,22 @@
 const ZoneVolumeSlider = ({
-    value = 50,
-    onChange,
+  value = 50,
+  onChange,
 }: {
-    value?: number;
-    onChange?: (val: number) => void;
+  value?: number;
+  onChange?: (val: number) => void;
 }) => {
-    return (
-        <div className="h-[80px] w-[20px] flex justify-center items-center ml-2">
-            <input
-                type="range"
-                min={0}
-                max={100}
-                defaultValue={value}
-                onChange={(e) => onChange?.(parseInt(e.target.value))}
-                className="appearance-none w-1 h-full bg-gradient-to-b from-cyan-400 to-blue-600 rounded-full"
-                style={{
-                    writingMode: "vertical-lr",
-                    WebkitAppearance: "slider-vertical",
-                }}
-            />
-        </div>
-    );
+  return (
+    <div className="w-full flex justify-center items-center ml-2">
+      <input
+        type="range"
+        min={0}
+        max={100}
+        defaultValue={value}
+        onChange={(e) => onChange?.(parseInt(e.target.value))}
+        className="w-full h-1 bg-white rounded-full"
+      />
+    </div>
+  );
 };
 
 export default ZoneVolumeSlider;
