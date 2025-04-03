a3ecbe650d27aadf2bc87170c983b52098b2719a
LuckyBoy012359 <rorifloris@gmail.com>
2025-04-03 01:06:57 +0100

    fix: add mute button to card volumn setting

--- src/pages/Music/components/ZoneVolumeSlider.tsx
+++ src/pages/Music/components/ZoneVolumeSlider.tsx
@@ -1,20 +1,29 @@
-const ZoneVolumeSlider = ({
-  value = 50,
-  onChange,
-}: {
-  value?: number;
-  onChange?: (val: number) => void;
-}) => {
+import { useState } from "react";
+import { IoVolumeMute, IoVolumeMedium } from "react-icons/io5";
+
+const ZoneVolumeSlider = () => {
+  const [volumnVal, setVolumnVal] = useState<number>(50);
   return (
-    <div className="w-full flex justify-center items-center ml-2">
+    <div className="w-full flex justify-center items-center px-2 gap-3">
       <input
         type="range"
         min={0}
         max={100}
-        defaultValue={value}
-        onChange={(e) => onChange?.(parseInt(e.target.value))}
+        defaultValue={volumnVal}
+        onChange={(e) => setVolumnVal(Number(e.target.value))}
         className="w-full h-1 bg-white rounded-full"
       />
+      {volumnVal > 0 ? (
+        <IoVolumeMute
+          className="cursor-pointer text-xl text-blue-500"
+          onClick={() => setVolumnVal(0)}
+        />
+      ) : (
+        <IoVolumeMedium
+          className="cursor-pointer text-xl text-blue-500"
+          onClick={() => setVolumnVal(100)}
+        />
+      )}
     </div>
   );
 };
