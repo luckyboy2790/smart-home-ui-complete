66c6c3161dce4fe09c138ccecd7d021c80906b8b
luckyboy2790 <rorifloris78@gmail.com>
2025-05-26 13:29:51 +0100

    update: complete main container fade animation

--- src/containers/MainContainer.tsx
+++ src/containers/MainContainer.tsx
@@ -1,11 +1,21 @@
 import { Main } from "../components";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const MainContainer = () => {
+  const location = useLocation();
+
   return (
     <Main>
       <Main.Container>
-        <Outlet />
+        <motion.div
+          key={location.pathname} // triggers animation on route change
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+        >
+          <Outlet />
+        </motion.div>
       </Main.Container>
     </Main>
   );
