c6834aa1bc07dda6c4b819da61ed6e4744fb282e
luckyboy2790 <rorifloris78@gmail.com>
2025-05-27 15:54:25 +0100

    update: update images link and folder location of background images

--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -13,26 +13,29 @@ import { useState } from "react";
 const Layout = () => {
   const location = useLocation();
 
-  const [room, setRoom] = useState("/src/assets/background-livingroom.jpg");
+  const [room, setRoom] = useState("/assets/background-livingroom.jpg");
 
   const handleChange = (value: string) => {
     if (value === "Living Room") {
-      setRoom("/src/assets/background-livingroom.jpg");
+      setRoom("/assets/background-livingroom.jpg");
     } else if (value === "Bathroom") {
-      setRoom("/src/assets/background-bathroom.jpg");
+      setRoom("/assets/background-bathroom.jpg");
     } else if (value === "Kitchen") {
-      setRoom("/src/assets/background-kitchen.jpg");
+      setRoom("/assets/background-kitchen.jpg");
     } else if (value === "Bedroom") {
-      setRoom("/src/assets/background-bedroom.jpg");
+      setRoom("/assets/background-bedroom.jpg");
     } else if (value === "Garage") {
-      setRoom("/src/assets/background-garage.jpg");
+      setRoom("/assets/background-garage.jpg");
     }
   };
 
   return (
     <div className="relative 2xl:px-30 xl:px-20 px-10">
       <div
-        className={`absolute top-0 left-0 bg-[url(${room})] w-screen h-screen bg-center bg-cover`}
+        style={{
+          backgroundImage: `url(${room})`,
+        }}
+        className="absolute top-0 left-0 w-screen h-screen bg-center bg-cover"
       ></div>
       <AppLayout>
         <ToastContainer />
